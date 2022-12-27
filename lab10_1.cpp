cdf38ffbb3c25b40c59ff76b51f80ce0f1cd6117
SukriTM <micsu.ky@gmail.com>
2022-12-27 16:17:34 +0700

    Lab10 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main(){
+    double PrevBalance ,Y,Payment;
+        cout << "Enter initial loan: ";
+        cin >> PrevBalance ;
+        cout << "Enter interest rate per year (%): ";
+        cin >> Y;
+        cout << "Enter amount you can pay per year: ";
+        cin >> Payment;
+    double Total, Interest, NewBalance;
+int EndOfYear=0;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +20,51 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+
+	do
+	{
+		EndOfYear+=1;
+		PrevBalance=PrevBalance;
+	    Interest=PrevBalance*(Y/100);
+		Total=PrevBalance+Interest;
+		if (Total>Payment)
+		{
+			NewBalance=Total-Payment;
+		}else if (Total<Payment)
+		{
+			Payment=Total;
+			NewBalance=Total-Total;
+		}
+		
+		
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << EndOfYear; 
+		cout << setw(13) << left << PrevBalance ;
+		cout << setw(13) << left << Interest;
+		cout << setw(13) << left << Total;
+		cout << setw(13) << left << Payment;
+		cout << setw(13) << left << NewBalance;
+		cout << "\n";
+
+		if (NewBalance>0)
+		{
+			PrevBalance=NewBalance;
+		}else if (NewBalance==0)
+		{
+			break;
+		}
+		
+	} while (1);
+
+return 0;
+}
+
+
+	//use 'setw' to set width of table and 'left' to set left-alignment
+	//you can change input argument of 'setw()' to see the effect
+	//Try to change from 'left' to 'right' and see the effect
+
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
-	return 0;
-}
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -3,22 +3,24 @@ using namespace std;
 
 //Write the function printO() here
 
-int main(){
+void printO(int x, int y){
+	int N=x, M=y;
+	if (N>0 && M>0)
+	{
+		
+		for (int i = 0; i < N; i++)
+		{
+			for (int i = 0; i < M; i++)
+			{
+				cout << 'O';
+			}
+		
+			cout << '\n';
+		}
+
+	}else cout << "Invalid input";
 	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
 }
+
+
+
